39e9dae621edaf42c2ea53446a92a46d13ac4e92
Felipe Pereira <pereira_felipesilva@outlook.com>
2019-06-09 01:47:27 -0300

    organização da estrutura do código

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
 const botaoSomar = document.querySelector('.botao-somar')
 
-botaoSomar.addEventListener('click', function(){
+botaoSomar.addEventListener('click', ()=>{
    let input1 = document.querySelector('#numero1')
    let input2 = document.querySelector('#numero2')
-   let resultado = document.querySelector('.resultado')
+   let resultado = document.querySelector('#resultado')
+   let soma = Number(input1.value) + Number(input2.value)
 
-   let resposta = Number(input1.value) + Number(input2.value)
-   resultado.textContent = resposta
+   resultado.value = soma
 })
