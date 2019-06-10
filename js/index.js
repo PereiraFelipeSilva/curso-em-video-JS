d59978c9d0ea1abc77764c4fffdb7902581c15bc
Felipe Pereira <pereira_felipesilva@outlook.com>
2019-06-10 20:28:45 -0300

    identificação dinâmica do horário do sistema do usuário

--- js/index.js
+++ js/index.js
@@ -1,10 +1,26 @@
-const botaoSomar = document.querySelector('.botao-somar')
+document.addEventListener('DOMContentLoaded', function(){
+   let saudacao = document.querySelector('.titulo-header')
+   let mensagem = document.querySelector('.mensagem')
+   let imagem = document.querySelector('#img')
+   let horas = new Date().getHours()
+   let minutos = new Date().getMinutes()
 
-botaoSomar.addEventListener('click', ()=>{
-   let input1 = document.querySelector('#numero1')
-   let input2 = document.querySelector('#numero2')
-   let resultado = document.querySelector('#resultado')
-   let soma = Number(input1.value) + Number(input2.value)
+   mensagem.innerHTML = `Agora são ${horas} horas e ${minutos} minutos`
 
-   resultado.value = soma
+   if(horas > 0 && horas < 12){
+      saudacao.innerHTML = 'Bom dia!'
+      imagem.src = 'img/manha.jpg'
+      imagem.alt = 'foto manhã'
+      document.body.style.backgroundColor = '#e2cd9f'
+   } else if(horas >= 12 && horas < 18){
+      saudacao.innerHTML = 'Boa tarde!'
+      imagem.src = 'img/tarde.jpg'
+      imagem.alt = 'foto tarde'
+      document.body.style.backgroundColor = '#b9846f'
+   } else {
+      saudacao.innerHTML = 'Boa noite'
+      imagem.src = 'img/noite.jpg'
+      imagem.alt = 'foto noite'
+      document.body.style.backgroundColor = '#04283e'
+   }
 })
